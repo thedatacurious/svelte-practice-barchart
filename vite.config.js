8177e01a7bc88396f8c305c1616d5450a78baa49
Alexandra Khoo <thedatacurious@gmail.com>
2022-11-29 15:24:43 +0800

    Debug attempt for error on big integer literals

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	optimizeDeps: {
-		esbuild: {
-			target: 'es2021'
-		}
-	},
 
+export default defineConfig({
 	build: {
-		outDir: 'build'
+		target: 'es2020'
 	},
-	plugins: [svelte()]
+	optimizeDeps: {
+		esbuildOptions: {
+			target: 'es2020'
+		}
+	}
 });
